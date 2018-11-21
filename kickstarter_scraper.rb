3dcc04de3e79e44b7e43e677c585e58187ff0293
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-21 20:17:56 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -9,7 +9,7 @@ def create_project_hash
 
   kickstarter.css("li.project.grid_4").each do |project|
     title = project.css("h2.bbcard_name strong a").text
-    image_link = project.css(".project-thumbnail a img").attribute("src").value
+    image_link = project.css("div.project-thumbnail a img").attribute("src").value
     projects[title.to_sym] = {
       image_link: image_link,
       description: project.css("p.bbcard_blurb").text
