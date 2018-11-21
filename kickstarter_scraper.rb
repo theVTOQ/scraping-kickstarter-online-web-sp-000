c0ed18a500ebaaeb938b0735979453e5fbbb85fe
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2018-11-21 19:59:33 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -7,5 +7,12 @@ def create_project_hash
   kickstarter = Nokogiri::HTML(html)
   projects = {}
 
+  kickstarter.css("li.project.grid_4").each do |project|
+    title = project.css("h2.bbcard_name strong a").text
+    projects[title] = {}
+    projects[title][:image_link] =
+    projects[title][:description] =
+    projects[title][:percent_funded] =
+  end
   projects
 end
